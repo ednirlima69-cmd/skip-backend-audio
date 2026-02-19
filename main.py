f8dc8bceae088a629b87d8c49ca9d1bbe7f17a2e
Ednir <seuemail@gmail.com>
2026-02-19 13:38:22 -0300

    Primeiro backup do backend de áudio

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import requests
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from fastapi.responses import StreamingResponse
+
+load_dotenv()
+
+app = FastAPI()
+
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
+
+@app.get("/")
+def root():
+    return {"status": "backend com voz funcionando"}
+
+@app.get("/falar")
+def falar(texto: str):
+    url = "https://api.elevenlabs.io/v1/text-to-speech/EXAVITQu4vr4xnSDxMaL"
+
+    headers = {
+        "xi-api-key": ELEVENLABS_API_KEY,
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "text": texto,
+        "voice_settings": {
+            "stability": 0.5,
+            "similarity_boost": 0.5
+        }
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+
+    return StreamingResponse(
+        iter([response.content]),
+        media_type="audio/mpeg"
+    )
